24455a039fa2296f532f568bdf906c5f189c451c
haitaa <mustafahaitaa@gmail.com>
2024-12-05 21:23:46 +0300

    Sidebar & Home Page comp

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -5,7 +5,7 @@ import Message from "../models/message.model.js";
 
 export const getUserForSidebar = async (req, res) => {
   try {
-    const loggedInUserId = req.user_id;
+    const loggedInUserId = req.user._id;
     const filteredUsers = await User.find({
       _id: { $ne: loggedInUserId },
     }).select("-password");
